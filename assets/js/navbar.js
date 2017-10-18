077199c33a2b6de8f5c73da2097cfaa2cf0dc724
unabris <u.abrisketa@epages.com>
2017-10-18 10:48:25 +0200

    Don't close search input on search view

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -39,13 +39,13 @@ $(document).ready(function() {
   function toggleSearchbar() {
     $('.navigation__search-button').click(function() {
       $('.navigation__search-input').addClass('navigation__search-input--open');
-    })
+    });
   }
 
   $(document).mouseup(function(e) {
     var container = $('.navigation__search-input');
 
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
+    if (!container.is(e.target) && container.has(e.target).length === 0 && !(window.location.href.indexOf('search') !== -1)) {
       container.removeClass('navigation__search-input--open');
     }
   });
