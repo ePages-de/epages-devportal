912636a4143163a001a6c951d4ff41a46250c96a
unabris <u.abrisketa@epages.com>
2017-10-18 16:48:25 +0200

    Configure lightbox

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-
-  wrapAnchorAroundImage();
-
-  function wrapAnchorAroundImage() {
-    $('.card--markdown img').each(function(index) {
-      $(this).wrap("<a href='" + this.src + "' data-lightbox='image-" + index + "'></a>");
-    });
-  }
+lightbox.option({
+  'disableScrolling': true
 });
