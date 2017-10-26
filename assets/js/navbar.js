364b60effd8e11327f5dd095a63c57f5edb52dbc
Christoph Müller <christoph.mueller@epages.com>
2017-10-26 12:38:24 +0200

    Change navbar color from white to blue

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -10,13 +10,9 @@ $(document).ready(function() {
     var vh = 100;
 
     if ($(window).scrollTop() < vh) {
-      $('body > .navbar').removeClass('navbar--white').addClass('navbar--transparent');
-      $('body > .navbar .navbar-wrapper a .logo').attr('src', '/assets/img/logo_developer_portal_white.svg');
-      $('body > .navbar .navbar-wrapper .navigation__button img').attr('src', '/assets/img/icons/menu_white.svg');
+      $('body > .navbar').removeClass('navbar--dark').addClass('navbar--transparent');
     } else {
-      $('body > .navbar').removeClass('navbar--transparent').addClass('navbar--white');
-      $('body > .navbar .navbar-wrapper a .logo').attr('src', '/assets/img/logo_developer_portal_4c.svg');
-      $('body > .navbar .navbar-wrapper .navigation__button img').attr('src', '/assets/img/icons/menu_gray.svg');
+      $('body > .navbar').removeClass('navbar--transparent').addClass('navbar--dark');
     }
   }
 
