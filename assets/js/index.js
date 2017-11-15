3e4fbfac0ad15a05edc0e20baab254782eccb51a
Christoph Müller <christoph.mueller@epages.com>
2017-11-15 13:49:40 +0100

    Refactor index page and exlude js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,29 @@
 ---
 ---
 
-{% capture signup_form %}{% include components/signup-form.html %}{% endcapture%}
+{% capture signup_html %}{% include components/signup-form.html %}{% endcapture%}
 
 $(document).ready(function() {
 // Show the popup when you click on the popup button
-$("a.popup").click(function(e){
+$('a[href="#signup"]').click(function(e){
     e.preventDefault();
 
     $.magnificPopup.open({
     items: {
-        src: "{{ signup_form | strip_newlines | replace: '"', "'" }}",
+        src: "{{ signup_html | strip_newlines | replace: '"', "'" }}",
         type: 'inline'
-    },
-    callbacks: {
-        close: function() {
-        cleanForms();
-        $('.modal #response-text').hide();
-        $('.modal #main-text').show();
-        }
     }
     });
 });
+
+$.magnificPopup.instance.close = function () {
+    if (!$('.load-spinner').is(':visible')) {
+    $.magnificPopup.proto.close.call(this);
+    cleanForms();
+    $('.modal #response-text').hide();
+    $('.modal #main-text').show();
+    }
+};
 
 $(document).on('submit', '#demoshopform-now', function() {
     $('.modal .load-spinner').show();
@@ -37,7 +39,6 @@ function cleanForms() {
     $('.form__input').val("");
     $('.form__input').val("");
     $('input[type="checkbox"]').attr('checked', false);
-    // Remove yellow color from autocompleted input fields
     if (navigator.userAgent.toLowerCase().indexOf('chrome') >= 0) {
     $('input:-webkit-autofill').each(function(){
         var text = $(this).val();
