50573e96930e86e4dcbbacf550bcbea58e9a079d
Christoph Müller <christoph.mueller@epages.com>
2017-11-16 11:37:38 +0100

    Add scroll up arrow to blog overview and blog posts

---
+++ assets/js/scroll-top.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.arrow-up').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 800, 'swing');
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop()>500) {
+      $('.arrow-up a').fadeIn();
+    } else {
+      $('.arrow-up a').fadeOut();
+    }
+  });
+});
