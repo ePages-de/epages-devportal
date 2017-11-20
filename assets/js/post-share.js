5b0d771ec90f8eae59dd21980fd64898000b17e8
Christoph Müller <christoph.mueller@epages.com>
2017-11-20 13:24:30 +0100

    Put if condition into a function

--- assets/js/post-share.js
+++ assets/js/post-share.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+
+  togglePostShare();
+
   $(window).scroll(function() {
+    togglePostShare();
+  });
+
+  function togglePostShare() {
     if ($('.post-share').overlaps('.header, .footer').length === 0) {
       $('.post-share').css('opacity', '1');
     } else {
       $('.post-share').css('opacity', '0');
     }
-  });
+  }
 });
