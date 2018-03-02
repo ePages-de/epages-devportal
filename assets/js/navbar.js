2ff735f7860268397eb7baf414759973c7347b3d
unabris <u.abrisketa@epages.com>
2018-03-02 13:50:28 +0100

    Add arrow functionality and adapt styles

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
     navbarScroll();
   });
 
+  if ($(window).width() <= 768) {
+    $('.navigation__link--disabled').click(function() {
+      $('svg', this).toggleClass('fa-caret-down').toggleClass('fa-caret-up');
+      if ($('svg', this).hasClass('fa-caret-up')) {
+        $(this).siblings('.dropdown').slideDown();
+      } else {
+        $(this).siblings('.dropdown').slideUp();
+      }
+    });
+  }
+
   function navbarScroll() {
     var vh = 100;
 
