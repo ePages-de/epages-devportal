140155a6dd17375370ebe9f68978f0ded65b25cf
unabris <u.abrisketa@epages.com>
2018-11-08 13:50:59 +0100

    Add tests to sidebar

--- _plugins/sidebar.rb
+++ _plugins/sidebar.rb
@@ -18,7 +18,12 @@ module Jekyll
       @result += "<ul class='#{@result.empty? ? 'sidebar' : 'sidebar__section'}'>"
       entries.each do |entry|
         if entry['entries'].nil?
+          raise "Sidebar: You need to provide a link for title: '#{entry['title']}'" if entry['link'].nil?
+          raise "Sidebar: You need to provide a title for title: '#{entry['link']}'" if entry['title'].nil?
+
           page = @context.registers[:site].pages.detect { |p| p.path == entry['link'] }
+
+          raise "Sidebar: Provided link doesn't exist -> #{entry['link']}" if page.nil?
           @result += "<li class='sidebar__link'><a href='#{page.permalink || page.url}'>#{entry['title']}</a></li>"
         else
           @result += "<li class='sidebar__button'>#{entry['title']}<i class='fas fa-caret-down sidebar__arrow'></i></li>"
