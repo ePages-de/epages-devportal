568850f535fc48ec850e8cbbf1d10c8cd4c1c888
unabris <u.abrisketa@epages.com>
2019-03-28 14:36:39 +0100

    Add events page

---
+++ assets/js/events.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  // Carousel
+  $('.js-carousel--about').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  // Lightbox
+  $('.js-lightbox__youtube').magnificPopup({
+    type: 'iframe'
+  });
+
+  $('.js-lightbox__image').magnificPopup({
+    type: 'image'
+  });
+});
