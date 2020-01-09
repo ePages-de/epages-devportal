a391ef681ad53dc4b4a80692050712f2cf37b9d4
unabris <u.abrisketa@epages.com>
2020-01-09 14:35:57 +0100

    Add quote plugin

---
+++ _plugins/quote.rb
@@ -0,0 +1,26 @@
+module Jekyll
+  class Quote < Liquid::Tag
+
+    ALLOWED_ATTRIBUTES = %w(
+      text
+    ).freeze
+
+    def initialize(_, args, _)
+      super
+      @args = OptionParser.parse(args, ALLOWED_ATTRIBUTES)
+      raise 'Quote: "text" attribute must be provided' if @args[:attributes][:text].nil?
+    end
+
+    def render(context)
+      %{<div class="quote">
+          <div class="quote__text">
+            #{@args[:attributes][:text]}
+            <i class="quote__icon quote__icon--left fas fa-quote-left"></i>
+            <i class="quote__icon quote__icon--right fas fa-quote-right"></i>
+          </div>
+        </div>}
+    end
+  end
+end
+
+Liquid::Template.register_tag('quote', Jekyll::Quote)
