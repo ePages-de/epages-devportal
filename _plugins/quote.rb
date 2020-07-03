c079142db227ad41543982132093ed65c57faf45
k4th <k.salazar@epages.com>
2020-07-03 12:55:45 +0200

    Override blockquote markdown

--- _plugins/quote.rb
+++ _plugins/quote.rb
@@ -2,15 +2,18 @@ module Jekyll
   class Quote < Liquid::Block
 
     def render(context)
+      Jekyll::Quote.render_with_quotes(Kramdown::Document.new(super).to_html)
+    end
+
+    def self.render_with_quotes(html_element)
       %{<div class="quote">
           <div class="quote__text">
-            #{Kramdown::Document.new(super).to_html}
+            #{html_element}
             <i class="quote__icon quote__icon--left fas fa-quote-left"></i>
             <i class="quote__icon quote__icon--right fas fa-quote-right"></i>
           </div>
         </div>}
     end
-
   end
 end
 
